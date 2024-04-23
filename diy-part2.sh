8d6f174a9509d559c8e6cb4586525a8dab5e0334
apan2380 <apan280@qq.com>
2024-04-23 15:51:30 +0800

    modify themes

--- diy-part2.sh
+++ diy-part2.sh
@@ -60,3 +60,35 @@ sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai
 #sed -i 's/^\tgrep.*vermagic/\tcp -f \$(TOPDIR)\/\.vermagic \$(LINUX_DIR)\/\.vermagic/g' include/kernel-defaults.mk
 #grep vermagic -n5 include/kernel-defaults.mk
 
+
+
+# 删除自定义源默认的 argon 主题
+rm -rf package/lean/luci-theme-argon
+rm -rf   feeds/luci/themes/luci-theme-argon
+
+# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
+find ./ -name luci-theme-argon | xargs rm -rf
+
+# 拉取 argon 源码
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+
+#其他第三方package
+git clone https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-opentopd
+#git clone https://github.com/sirpdboy/netspeedtest package/netspeedtest
+#git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
+git clone https://github.com/sirpdboy/luci-app-advanced package/luci-app-advanced
+#git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+
+#取消bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 替换默认主题为 luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
+#删除默认密码
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
+# Modify default IP
+#sed -i 's/192.168.1.1/168.168.1.3/g' package/base-files/files/bin/config_generate
+
